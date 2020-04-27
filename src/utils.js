c221cfebf1260598fa763227dc7bc1afac526897
Matt Moore <matt.e.moore@gmail.com>
2020-04-27 08:04:04 -0500

    removed console.log

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,4 @@
 exports.doGET = async (httpClient, url) => {
-    console.log(url);
     return new Promise((resolve, reject) => {
         httpClient.get(url, (error, response, body) => {
             if (error || response.statusCode < 200 || response.statusCode >= 300) reject (error || body);
